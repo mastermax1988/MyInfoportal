06759bc9ff478b1331986221024ca73c810e5f41
max pc <huber.maxi@gmail.com>
2021-02-12 20:54:08 +0100

    autologin working :)

--- app/src/main/java/com/example/myinfoportal/MainActivity.java
+++ app/src/main/java/com/example/myinfoportal/MainActivity.java
@@ -8,6 +8,10 @@ import android.accounts.AccountManagerCallback;
 import android.accounts.AccountManagerFuture;
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -19,13 +23,19 @@ import org.jboss.aerogear.security.otp.Totp;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Totp generator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        generator = new Totp(BuildConfig.HOTP_KEY);
+        Totp generator = new Totp(BuildConfig.TOTP_KEY);
         TextView txt = findViewById(R.id.txt);
         txt.setText("Login-Token:\n" + generator.now());
+        ClipboardManager cp = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        cp.setPrimaryClip(ClipData.newPlainText("",generator.now()));
+        findViewById(R.id.button).setOnClickListener((e)->{
+            Intent intent = new Intent(this,WebView.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/myinfoportal/WebView.java
+++ app/src/main/java/com/example/myinfoportal/WebView.java
@@ -3,12 +3,47 @@ package com.example.myinfoportal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebViewClient;
+
+import org.jboss.aerogear.security.otp.Totp;
 
 public class WebView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_web_view);
+        android.webkit.WebView webView = findViewById(R.id.wv);
+        Totp generator = new Totp(BuildConfig.TOTP_KEY);
+        webView.getSettings().setJavaScriptEnabled(true);
+        final int[] iMaxTries = {2}; //necessary for access in inner class
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(android.webkit.WebView view, String url) {
+                if(iMaxTries[0] <=0)
+                    return;
+                iMaxTries[0]--;
+                System.out.println(url);
+                if(!url.contains("login"))
+                {
+                    System.out.println("no login page! aborting");
+                    return;
+                }
+                super.onPageFinished(view, url);
+                String myCustomJS = "try{document.getElementById('user').value='"+BuildConfig.USERNAME+"'; " +
+                        "document.getElementById('password').value='"+BuildConfig.PASSWORD+"'; }catch(e){} " +
+                        "try{document.getElementById('otp').value='"+generator.now()+"';}catch(e){} " +
+                        "document.getElementById('submit_button').click();";
+                System.out.println(myCustomJS);
+                webView.loadUrl("javascript:(function(){" + myCustomJS + "})()");
+            }
+        });
+
+        webView.loadUrl("https://schule-infoportal.de/login/wggymuc");
     }
 }
