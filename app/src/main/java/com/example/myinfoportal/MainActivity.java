00e3fd736aa4444af19b2c3ef5b019fc47d871b8
max pc <huber.maxi@gmail.com>
2021-02-13 14:11:11 +0100

    parsing postkorb

--- app/src/main/java/com/example/myinfoportal/MainActivity.java
+++ app/src/main/java/com/example/myinfoportal/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myinfoportal;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.accounts.AccountManager;
@@ -12,6 +13,7 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -20,22 +22,64 @@ import android.widget.TextView;
 
 import org.jboss.aerogear.security.otp.Totp;
 
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Totp generator = new Totp(BuildConfig.TOTP_KEY);
-        TextView txt = findViewById(R.id.txt);
-        txt.setText("Login-Token:\n" + generator.now());
-        ClipboardManager cp = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        cp.setPrimaryClip(ClipData.newPlainText("",generator.now()));
+
+        updateToken();
         findViewById(R.id.button).setOnClickListener((e)->{
             Intent intent = new Intent(this,WebView.class);
             startActivity(intent);
         });
+        findViewById(R.id.btnBrowser).setOnClickListener((e)->
+        {
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://schule-infoportal.de/login/wggymuc"));
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.setPackage("com.android.chrome");
+            startActivity(i);
+        });
+        Timer timer = new UpdateTimer();
     }
+    private void updateToken()
+    {
+        Totp generator = new Totp(BuildConfig.TOTP_KEY);
+        TextView txt = findViewById(R.id.txt);
+        String token = generator.now();
+        txt.setText("Login-Token:\n" + token +"\n(wurde kopiert)");
+        ClipboardManager cp = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        cp.setPrimaryClip(ClipData.newPlainText("Infoportal",token));
+    }
+
+    private class UpdateTimer extends Timer
+    {
+        public UpdateTimer()
+        {
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            updateToken();
+                        }
+                    });
+                }
+            };
+            schedule(task,10000,10000);
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+
 }
--- app/src/main/java/com/example/myinfoportal/WebView.java
+++ app/src/main/java/com/example/myinfoportal/WebView.java
@@ -1,39 +1,61 @@
 package com.example.myinfoportal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Window;
+import android.webkit.MimeTypeMap;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebViewClient;
 
 import org.jboss.aerogear.security.otp.Totp;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
 public class WebView extends AppCompatActivity {
 
+    private int iMaxTries;
+    private boolean parsingDone;
+    private String postkorb;
+    private android.webkit.WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_web_view);
-        android.webkit.WebView webView = findViewById(R.id.wv);
+        webView = findViewById(R.id.wv);
         Totp generator = new Totp(BuildConfig.TOTP_KEY);
         webView.getSettings().setJavaScriptEnabled(true);
-        final int[] iMaxTries = {2}; //necessary for access in inner class
+        iMaxTries = savedInstanceState == null? 2: savedInstanceState.getInt("iMaxTries");
+        parsingDone = savedInstanceState != null && savedInstanceState.getBoolean("parsingDone");
+        postkorb = savedInstanceState == null? "" : savedInstanceState.getString("postkorb");
+        webView.setWebContentsDebuggingEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(android.webkit.WebView view, String url) {
-                if(iMaxTries[0] <=0)
+                if(parsingDone)
                     return;
-                iMaxTries[0]--;
-                System.out.println(url);
                 if(!url.contains("login"))
                 {
-                    System.out.println("no login page! aborting");
-                    return;
+                    System.out.println("user seems to be logged in -> grabbing postkorb and logging out");
+                    webView.evaluateJavascript("(function() { return ('<html><table>'+document.getElementById('postkorb-box').innerHTML+'</table></html>'); })();", new CB());
+                    String myCustomJS = "document.getElementsByClassName(\"logout_button\")[0].click()";
+                    webView.loadUrl("javascript:(function(){" + myCustomJS + "})()");
+                    iMaxTries = 0;
+                    parsingDone = true;
+                    loadPostkorb();
                 }
+                if(iMaxTries <=0)
+                    return;
+                iMaxTries--;
+                System.out.println(url);
                 super.onPageFinished(view, url);
                 String myCustomJS = "try{document.getElementById('user').value='"+BuildConfig.USERNAME+"'; " +
                         "document.getElementById('password').value='"+BuildConfig.PASSWORD+"'; }catch(e){} " +
@@ -43,7 +65,39 @@ public class WebView extends AppCompatActivity {
                 webView.loadUrl("javascript:(function(){" + myCustomJS + "})()");
             }
         });
+        if(savedInstanceState == null)
+            webView.loadUrl("https://schule-infoportal.de/login/wggymuc");
+        else
+            loadPostkorb();
+    }
 
-        webView.loadUrl("https://schule-infoportal.de/login/wggymuc");
+
+    private synchronized void loadPostkorb()
+    {
+        webView.loadDataWithBaseURL(null,postkorb,"text/html","utf-8",null);
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("iMaxTries",iMaxTries);
+        outState.putBoolean("parsingDone", parsingDone);
+        outState.putString("postkorb",postkorb);
+    }
+    private class CB implements ValueCallback<String>
+    {
+        @Override
+        public void onReceiveValue(String value) {
+            Properties p = new Properties();
+            parsingDone = true;
+            try {
+                p.load(new StringReader("key="+value));
+                value = p.getProperty("key").replaceAll("\n","");
+                value = value.substring(1,value.length()-2);
+                postkorb = value;
+                loadPostkorb();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
